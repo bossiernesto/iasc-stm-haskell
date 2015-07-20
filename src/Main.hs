30151069cb3514914e11ce135aefb4e1d29e7dc0
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-20 11:59:38 -0300

    Naive sample

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,23 @@
-module Main where
+module Main (main) where
 
-  main :: IO ()
-  main = putStr "hola"
+import Control.Concurrent.Async
+
+data Account = Account Int deriving (Show, Eq)
+
+main :: IO ()
+main = do
+  let account = newAccount
+  task1 <- async (return $ deposit 10 account)
+  task2 <- async (return $ withdraw 6 account)
+  wait task1
+  wait task2
+  putStr $ show account
+  return ()
+
+newAccount = Account 0
+
+update f (Account n) = Account (f n)
+
+deposit amount = update (+amount)
+
+withdraw amount = update (flip (-) amount)
